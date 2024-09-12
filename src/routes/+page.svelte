cd6f801790a2d39b212f18d1256db21890c4db4d
D3asyn <konkolykovacsd@gmail.com>
2024-09-12 12:45:26 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+	var a = 0;
+	var b = 0;
+</script>
+
+<h1>Eredmeny jelzo</h1>
+
+<div>
+	<a href="/svelteora/test">Animation</a>
+</div>
+
+<table>
+	<tr>
+		<th>A</th>
+		<th>-</th>
+		<th>B</th>
+	</tr>
+	<tr>
+		<td>{a}</td>
+		<td>-</td>
+		<td>{b}</td>
+	</tr>
+	<tr>
+		<td colspan="3">
+			<button on:click={() => a++}>A noveles</button>
+			<button on:click={() => b++}>B noveles</button>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="3">
+			<button on:click={() => a--}>A csokkentes</button>
+			<button on:click={() => b--}>B csokkentes</button>
+		</td>
+	</tr>
+</table>
+
+<style>
+	td {
+		text-align: center;
+	}
+</style>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	const l = Array(100).fill(0);
+</script>
+
+<div class="g">
+	{#each l as _}
+		<button class="l{_}" on:click={() => (_ = _ == 1 ? 0 : 1)}></button>
+	{/each}
+</div>
+
+<style>
+	.g {
+		display: grid;
+		grid-template-columns: repeat(10, 1fr);
+	}
+	.g button {
+		padding: 10px;
+		border: 1px solid black;
+		text-align: center;
+		cursor: pointer;
+	}
+	.g button.l0 {
+		background-color: antiquewhite;
+	}
+	.g button.l1 {
+		background-color: rgb(167, 214, 200);
+	}
+</style>
